b8beee3651f7e688c479c82291edaf94cc77e36a
ali <ali.s.tag2000@gmail.com>
2022-12-18 12:29:45 +0330

    In this file we will get all inputs like soil properties and loads ( surcharge loads ). then we will use functions in lateral pressure directory to calculate lateral pressure for surcharge and active passive pressure. then we should draw load diagram. then we should calculate forces and moments due these load diagram. then calculate D, Do according to FS. then calculate Y0 --> where shear will be zero. calculate maximum moment. calculate S required. ( solve example 2 ) program will run good when there are no surcharge loads. then need to developed.

---
+++ soldier_pile/shoring_cantilever.py
@@ -0,0 +1,149 @@
+""" In this file we will get all inputs like soil properties and loads ( surcharge loads ).
+then we will use functions in lateral pressure directory to calculate lateral pressure for surcharge and active passive
+pressure.
+then we should draw load diagram.
+then we should calculate forces and moments due these load diagram.
+then calculate D, Do according to FS.
+then calculate Y0 --> where shear will be zero.
+calculate maximum moment.
+calculate S required.
+( solve example 2 )
+now for continue and solve example 3 I should pass deflection stuff."""
+import copy
+import sys
+
+from sympy import symbols
+from sympy.solvers import solve
+
+sys.path.append(r"D:/git/Shoring/Lateral-pressure-")
+
+from Passive_Active.active_passive import active_passive
+from Force.force import moment_calculator
+
+
+def calculate_force_and_arm(soil_pressure, water_pressure, state):
+    if state == "active":
+        main = main_active
+    else:
+        main = main_passive
+    force_soil, arm_soil = main.force_final(soil_pressure)
+    force_water, arm_water = main.force_final(water_pressure, "water")
+    for i in force_water:
+        force_soil.append(i)
+    for i in arm_water:
+        arm_soil.append(i)
+    return force_soil, arm_soil
+
+
+D = symbols("D")
+h_active = [10, D]
+main_active = active_passive([10, D], ["No", "No"])
+soil_active, water_active = main_active.pressure_calculator(number_of_layer=2, gama=[120, 125], phi=[34, 36],
+                                                            theory="Coulomb",
+                                                            state="active",
+                                                            unit_system="us", beta=[0, 0], omega=[0, 0], delta=[0, 24])
+
+force_soil_active, arm_soil_active = calculate_force_and_arm(soil_active, water_active, "active")
+
+h_passive = [D]
+main_passive = active_passive([D], ["No"])
+soil_passive, water_passive = main_passive.pressure_calculator(number_of_layer=1, gama=[125], phi=[36],
+                                                               theory="Coulomb",
+                                                               state="passive",
+                                                               unit_system="us", beta=[-32], omega=[0], delta=[24])
+
+force_soil_passive, arm_soil_passive = calculate_force_and_arm(soil_passive, water_passive, "passive")
+
+
+def control_solution(item):
+    final = []
+    for number in item:
+        try:
+            final.append(float(number))
+        except:
+            pass
+    if final:
+        final_value = max(final)
+    else:
+        final_value = "There is no answer!"
+    return final_value
+
+
+# function is ready when we have no surcharge loads.
+def cantilever_soldier_pile(unit_system, active_force, passive_force, active_arm, passive_arm, FS, pile_spacing, fy):
+    """units :
+    if unit system = us -->
+    forces -> lb
+    arm and pile spacing -> ft
+    fy -> ksi
+
+    if unit system --> metric
+    forces -> N
+    arm and pile spacing -> m
+    fy -> MPa"""
+    D = symbols("D")
+    Md = moment_calculator(active_force, active_arm, pile_spacing)  # driving moment
+    Ms = moment_calculator(passive_force, passive_arm, pile_spacing)  # resisting moment
+    equation = Ms - FS * Md
+
+    # finding D0
+    D_zero = solve(equation, D)
+    D_zero = control_solution(D_zero)
+    if D_zero != "There is no answer!":
+        D_final = 1.2 * D_zero
+    else:
+        D_final = "There is no answer!"
+        return "There is no answer for D0!"
+
+    # finding Y. where shear equal zero
+    active_force_sum = 0
+    for layer in active_force:
+        for force in layer:
+            active_force_sum += force
+
+    passive_force_sum = 0
+    for layer in passive_force:
+        for force in layer:
+            passive_force_sum += force
+    equation2 = passive_force_sum - active_force_sum
+    Y = solve(equation2, D)
+    Y = control_solution(Y)
+    if Y == "There is no answer!":
+        return "There is no answer for Y! ( where shear equal to zero )"
+    else:
+        active_force_zero_shear = copy.deepcopy(active_force)
+        active_arm_zero_shear = copy.deepcopy(active_arm)
+        for layer in range(len(active_force_zero_shear)):
+            for force in range(len(active_force_zero_shear[layer])):
+                try:
+                    active_force_zero_shear[layer][force] = active_force_zero_shear[layer][force].subs(D, Y)
+                    active_arm_zero_shear[layer][force] = active_arm_zero_shear[layer][force].subs(D, Y)
+                except:
+                    pass
+
+        passive_force_zero_shear = copy.deepcopy(passive_force)
+        passive_arm_zero_shear = copy.deepcopy(passive_arm)
+        for layer in range(len(passive_force_zero_shear)):
+            for force in range(len(passive_force_zero_shear[layer])):
+                try:
+                    passive_force_zero_shear[layer][force] = passive_force_zero_shear[layer][force].subs(D, Y)
+                    passive_arm_zero_shear[layer][force] = passive_arm_zero_shear[layer][force].subs(D, Y)
+                except:
+                    pass
+
+        M_max_active = moment_calculator(active_force_zero_shear, active_arm_zero_shear, pile_spacing)
+        M_max_passive = moment_calculator(passive_force_zero_shear, passive_arm_zero_shear, pile_spacing)
+        M_max = abs(M_max_passive - M_max_active)
+        M_max = M_max.subs(D, Y)
+        fb = 0.66 * fy
+        if unit_system == "us":
+            s_required = M_max * 12 / (fb * 1000)  # s unit --> inch^3
+        else:
+            s_required = M_max * 10 ** 6 / fb  # s unit --> mm^3
+
+    return "No Error!", D_zero, D_final, Y, M_max, s_required
+
+
+value = cantilever_soldier_pile("us", force_soil_active, force_soil_passive, arm_soil_active, arm_soil_passive, 1.3, 6,
+                                36)
+print(value)
