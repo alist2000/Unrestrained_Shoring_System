c23ad92daec8f697875d07141333f0c9904c1f54
ali <ali.s.tag2000@gmail.com>
2022-12-28 15:53:13 +0330

    calculate delta c/b. must be checked and control.

--- soldier_pile/deflection.py
+++ soldier_pile/deflection.py
@@ -12,4 +12,12 @@ import plotly.graph_objects as go
 from plotly.graph_objs import Layout
 
 
-def deflection_calculator(moment,E,I):
+def deflection_calculator(depth, moment, PoF, c):
+    # calculate delta C/B
+    c_point = depth.index(c)
+    PoF_point = depth.index(PoF)
+    # area = abs(spi.simpson(moment[c_point:PoF_point], depth[c_point:PoF_point]))
+    # X = abs(spi.simpson(moment[c_point:PoF_point] * depth[c_point:PoF_point], depth[c_point:PoF_point])) / area
+    # delta_cb = area * X
+    delta_cb = abs(spi.simpson(moment[c_point:PoF_point] * depth[c_point:PoF_point], depth[c_point:PoF_point]))
+    return delta_cb
