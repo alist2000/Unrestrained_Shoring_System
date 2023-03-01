57c2b29eb4526af149bf531f49c40bc13801ae55
ali <ali.s.tag2000@gmail.com>
2023-03-01 15:43:20 +0330

    site inputs.

---
+++ soldier_pile/site_input.py
@@ -0,0 +1,58 @@
+input1 = {'information': {'title': '1', 'jobNo': '1', 'designer': '1', 'checker': '1', 'company': '1', 'client': '1',
+                          'unit': 'us', 'date': None, 'comment': None, 'other': None}, 'product_id': 26, 'user_id': 44,
+          'data': {'General data': {'FS': {'value': '1', 'unit': None}, 'Fy': {'value': '36', 'unit': 'ksi'},
+                                    'E': {'value': '29000', 'unit': 'ksi'},
+                                    'Pile Spacing': {'value': '3', 'unit': 'ft'},
+                                    'Allowable Deflection': {'value': '0.5', 'unit': 'in'}, 'Sections': {
+                  'value': '{"id":1380,"item":"W44,W40,W36,W33,W30,W27,W24,W21,W18,W16,W14,W12,W10,W8X,W6,W5,W4","section_product_item":"39_91"}',
+                  'unit': None}},
+                   'Soil Properties': {'β active': {'value': '0', 'unit': '°'}, 'δ': {'value': '-1', 'unit': '°'},
+                                       'β passive': {'value': '0', 'unit': '°'},
+                                       'Equivalent Fluid Pressure Passive': {'value': '1', 'unit': 'pcf'},
+                                       'Φ': {'value': '-1', 'unit': '°'}, 'γ': {'value': '1', 'unit': 'pcf'},
+                                       'Height of layer': {'value': '1', 'unit': 'ft'}, 'Soil Layer Number': {
+                           'value': '{"id":1437,"item":"1","section_product_item":"41_94"}', 'unit': None},
+                                       'γ ': {'value': '1', 'unit': 'pcf'}, 'Formula': {
+                           'value': '{"id":1439,"item":"Rankine","section_product_item":"41_94"}', 'unit': None},
+                                       'Φ ': {'value': '1', 'unit': '°'},
+                                       'Retaining Height': {'value': '1', 'unit': 'ft'}}, 'Surcharge': {
+                  'Load Type': {'value': '{"id":1381,"item":"Uniform","section_product_item":"40_92"}', 'unit': None},
+                  'Load Type ': {'value': '{"id":1382,"item":"Uniform","section_product_item":"40_92"}', 'unit': None},
+                  'Load Type  ': {'value': '{"id":1383,"item":"Uniform","section_product_item":"40_92"}', 'unit': None},
+                  'Load Type   ': {'value': '{"id":1384,"item":"Uniform","section_product_item":"40_92"}',
+                                   'unit': None}, 'q  ': {'value': '1', 'unit': None},
+                  'q   ': {'value': '1', 'unit': None}, 'q ': {'value': '1', 'unit': None},
+                  'q': {'value': '1', 'unit': None}},
+                   'Lagging': {'Ph max': {'value': '1', 'unit': 'psf'}, 'Fb': {'value': '1', 'unit': 'ksi'},
+                               'Timber Size': {'value': '{"id":1403,"item":"1 x 2","section_product_item":"42_93"}',
+                                               'unit': None}}}, 'type': 1, 'number_of_projects': 1}
+input2 = {'information': {'title': '1', 'jobNo': '1', 'designer': '1', 'checker': '1', 'company': '1', 'client': '1',
+                          'unit': 'us', 'date': None, 'comment': None, 'other': None}, 'product_id': 26, 'user_id': 44,
+          'data': {'General data': {'FS': {'value': '1', 'unit': None}, 'Fy': {'value': '36', 'unit': 'ksi'},
+                                    'E': {'value': '29000', 'unit': 'ksi'},
+                                    'Pile Spacing': {'value': '6', 'unit': 'ft'},
+                                    'Allowable Deflection': {'value': '0.5', 'unit': 'in'}, 'Sections': {
+                  'value': '{"id":1380,"item":"W44,W40,W36,W33,W30,W27,W24,W21,W18,W16,W14,W12,W10,W8X,W6,W5,W4","section_product_item":"39_91"}',
+                  'unit': None}},
+                   'Soil Properties': {'β active': {'value': '0', 'unit': '°'}, 'δ': {'value': '-1', 'unit': '°'},
+                                       'β passive': {'value': '0', 'unit': '°'},
+                                       'Water started from top of this layer?': {'value': 'No', 'unit': None},
+                                       'Equivalent Fluid Pressure Passive': {'value': '1', 'unit': 'pcf'},
+                                       'Φ': {'value': '-1', 'unit': '°'}, 'γ': {'value': '1', 'unit': 'pcf'},
+                                       'Water started from top of this layer? ': {'value': 'No', 'unit': None},
+                                       'Height of layer': {'value': '1', 'unit': 'ft'}, 'Soil Layer Number': {
+                           'value': '{"id":1437,"item":"1","section_product_item":"41_94"}', 'unit': None},
+                                       'γ ': {'value': '1', 'unit': 'pcf'}, 'Formula': {
+                           'value': '{"id":1439,"item":"Rankine","section_product_item":"41_94"}', 'unit': None},
+                                       'Φ ': {'value': '1', 'unit': '°'},
+                                       'Retaining Height': {'value': '1', 'unit': 'ft'}}, 'Surcharge': {
+                  'Load Type': {'value': '{"id":1381,"item":"Uniform","section_product_item":"40_92"}', 'unit': None},
+                  'Load Type ': {'value': '{"id":1382,"item":"Uniform","section_product_item":"40_92"}', 'unit': None},
+                  'Load Type  ': {'value': '{"id":1383,"item":"Uniform","section_product_item":"40_92"}', 'unit': None},
+                  'Load Type   ': {'value': '{"id":1384,"item":"Uniform","section_product_item":"40_92"}',
+                                   'unit': None}, 'q  ': {'value': '1', 'unit': None},
+                  'q   ': {'value': '1', 'unit': None}, 'q ': {'value': '1', 'unit': None},
+                  'q': {'value': '1', 'unit': None}},
+                   'Lagging': {'Ph max': {'value': '1', 'unit': 'psf'}, 'Fb': {'value': '1', 'unit': 'ksi'},
+                               'Timber Size': {'value': '{"id":1403,"item":"1 x 2","section_product_item":"42_93"}',
+                                               'unit': None}}}, 'type': 1, 'number_of_projects': 1}
