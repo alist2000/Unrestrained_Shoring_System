0dcdc988f6e24c0c33661c31cfa0ba82f5c3f484
ali <ali.s.tag2000@gmail.com>
2023-03-16 13:23:02 +0330

    round excel numbers with 4 decimals. --> use lambda

--- soldier_pile/report.py
+++ soldier_pile/report.py
@@ -4,11 +4,7 @@ import pyarrow.feather as feather
 
 # creating excel
 def create_feather(z, value, title, excel_name):
-    value = list(map(round_4, value))
+    value = list(map(lambda x: round(x, 4), value))
     data = list(zip(z, value))
     df = pd.DataFrame(data, columns=["Z", title])
     df.to_feather("reports/excel/" + excel_name + ".feather")
-
-
-def round_4(i):
-    return round(i, 4)
