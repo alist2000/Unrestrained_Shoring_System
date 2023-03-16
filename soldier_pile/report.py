e0924a5b9a78f2a01ee247a56d08522a84d637c6
ali <ali.s.tag2000@gmail.com>
2023-03-16 13:03:04 +0330

    round excel numbers with 4 decimals.

--- soldier_pile/report.py
+++ soldier_pile/report.py
@@ -4,6 +4,11 @@ import pyarrow.feather as feather
 
 # creating excel
 def create_feather(z, value, title, excel_name):
+    value = list(map(round_4, value))
     data = list(zip(z, value))
     df = pd.DataFrame(data, columns=["Z", title])
     df.to_feather("reports/excel/" + excel_name + ".feather")
+
+
+def round_4(i):
+    return round(i, 4)
