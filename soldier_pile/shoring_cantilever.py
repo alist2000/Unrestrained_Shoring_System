ea019feb4ce0507b196e4a67ce30fb53714dcfd4
ali <ali.s.tag2000@gmail.com>
2023-05-17 17:19:48 +0430

    edit for report. not finished yet.

--- soldier_pile/shoring_cantilever.py
+++ soldier_pile/shoring_cantilever.py
@@ -85,6 +85,7 @@ def cantilever_soldier_pile(unit_system, h_active, h_passive, Surcharge_force, S
     Ms = moment_calculator(passive_force, passive_arm, pile_spacing)  # resisting moment
     equation = Ms - FS * (Md + Md_surcharge_total)
     equation2 = Ms - (Md + Md_surcharge_total)
+    equations_report = [Ms, Md + Md_surcharge_total, equation]
 
     # finding D0
     D_zero = solve(equation, D)
@@ -172,7 +173,7 @@ def cantilever_soldier_pile(unit_system, h_active, h_passive, Surcharge_force, S
             # s_required = M_max * 10 ** 6 / fb  # s unit --> mm^3
             s_required = M_max * 10 ** 3 / fb  # s unit --> mm^3  # fb: MPa = N/mm^2
 
-    return "No Error!", float(D_zero), float(D_final), float(Y), float(M_max), float(s_required), float(second_D_zero)
+    return "No Error!", float(D_zero), float(D_final), float(Y), float(M_max), float(s_required), float(second_D_zero), equations_report
 
 
 def put_D_in_list(my_list, d):
